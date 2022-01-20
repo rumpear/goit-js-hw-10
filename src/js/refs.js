f7378ae66274ea88b098d30988a324fdd24fee4e
Roman <77742141+rumpear@users.noreply.github.com>
2022-01-20 14:29:35 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  inputSearch: document.querySelector('#search-box'),
+  listCountry: document.querySelector('.country-list'),
+  infoCountry: document.querySelector('.country-info'),
+};
